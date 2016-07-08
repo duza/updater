c9c10df672636ca19e077795b31e1df1da5d9572
duza <duza@tut.by>
2016-07-08 19:52:02 +0300

    Deleted nonsense and maked lower symbols

--- resumeupdater.py
+++ resumeupdater.py
@@ -1,12 +1,10 @@
 import requests
 from lxml import html
 
-def upresume(logn, pswd, resume_name):
+def upresume(login, paswd, resume_name):
     with requests.Session() as c:
         url = 'https://jobs.tut.by/account/login'
-        USERNAME = logn
-        PASSWORD = pswd
-        HEADERS = {'Referer': 'https://jobs.tut.by/account/login',\
+        headers = {'Referer': 'https://jobs.tut.by/account/login',\
     'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',\
     'Host':'jobs.tut.by',\
     'Accept-Encoding':'gzip, deflate, br',\
@@ -15,17 +13,18 @@ def upresume(logn, pswd, resume_name):
     'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:47.0) Gecko/20100101 Firefox/47.0',\
     'DNT':'1'
     }
-        c.get(url,headers=HEADERS)
+        c.get(url,headers=headers)
         token = c.cookies['_xsrf']
-        login_data = dict(username=USERNAME,\
-    password=PASSWORD,\
+        login_data = dict(username=login,\
+    password=paswd,\
     backUrl='https://jobs.tut.by/',\
     failUrl='/account/login?backurl=%2F&role=',\
     _xsrf=token
     )
-        c.post(url, data=login_data, headers=HEADERS)
+        c.post(url, data=login_data, headers=headers)
         c.get('https://jobs.tut.by/account/login')
         page = c.get('https://jobs.tut.by/applicant/resumes')
+        page.encoding = "utf-8"
         tree = html.fromstring(page.content)
         all_links = tree.xpath('//a[@href]')
         for link in all_links:
@@ -34,13 +33,12 @@ def upresume(logn, pswd, resume_name):
                     resume_link = link
             except Exception as err:
                  raise err
-    #    print resume_link.attrib['href']
         url = 'https://jobs.tut.by' + resume_link.attrib['href']
         id_resume = url.split('/')[-1]
         c.get(url)
         params_data = {'resume': id_resume, 'undirectable':'true'}
         url_update = 'https://jobs.tut.by/applicant/resumes/touch'
-        CHANGES = {
+        changes = {
     'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',\
     'X-Xsrftoken': c.cookies['_xsrf'],\
     'X-Requested-With': 'XMLHttpRequest',\
@@ -48,7 +46,7 @@ def upresume(logn, pswd, resume_name):
     'Accept': '*/*',\
     'Content-Length': '63'
     }
-        HEADERS.update(CHANGES)
-        r = c.post(url=url_update, data=params_data, headers=HEADERS)
+        headers.update(changes)
+        r = c.post(url=url_update, data=params_data, headers=headers)
         page = c.get(url)
-        return r.status_code, page.content  
+        return r.status_code, page.content
